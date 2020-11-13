91600412a749ef67d09f33e3718e0e5e0a61a53c
almayor <mayorovme@gmail.com>
2020-11-13 10:44:08 +0300

    fixed logging in st_instruct

--- src/instruct/st_instruct.c
+++ src/instruct/st_instruct.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 14:38:07 by user              #+#    #+#             */
-/*   Updated: 2020/11/12 22:20:08 by user             ###   ########.fr       */
+/*   Updated: 2020/11/13 10:42:25 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,14 @@ int	st_instruct(t_proc *proc)
 		return (1);
 	type = get_arg_type(proc, 1);
 	if ((g_vm.log >> 2) & 1)
-		ft_printf(
-			LOG_OPER_PREFIX "store at %i + (%i %% %i) = %i\n"
-			LOG_OPER_PREFIX "storing %i\n",
-			proc->pc, proc->args[1], IDX_MOD,
-			proc->pc + (proc->args[1] % IDX_MOD), in);
+	{
+		if (type == T_IND)
+			ft_printf(LOG_OPER_PREFIX "store at %i + (%i %% %i) = %i\n",
+				proc->pc, proc->args[1], IDX_MOD,
+				proc->pc + (proc->args[1] % IDX_MOD));
+		else
+			ft_printf(LOG_OPER_PREFIX "store at r%i\n", proc->args[1]);
+		ft_printf(LOG_OPER_PREFIX "storing %i\n", in);
+	}
 	return (put_argument(proc, 1, in));
 }
