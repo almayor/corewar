bdfa1393631498af8de8438a7e5c3246638e5d90
iskravtsov <iskravtsov@avito.ru>
2020-11-21 21:35:50 +0300

    merge commit

---
+++ batman_new.s
@@ -0,0 +1,19 @@
+.name       "Batman"
+.comment    "This city needs me"
+loop:
+        sti r1, %:live, %1		# comment
+live:
+	live %0
+		zjmp %:typing
+		sti     r1,%:copie,%2
+		ld %0, r2
+label1: label: ld %0, r2
+		sub	r2,r4,r2
+		zjmp :loop
+		zjmp %:loop
+		st r3, r5
+
+typing: aff r2		
+	   
+copie:	ld  %1,r1
+		xor	r10,r10,r10
