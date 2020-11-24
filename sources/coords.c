7397219cce4815a906e8d14aaa6d11d5e4e79d30
iskravtsov <iskravtsov@avito.ru>
2020-11-24 21:45:52 +0300

    upd error messages on labels

--- sources/coords.c
+++ sources/coords.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 17:44:40 by kysgramo          #+#    #+#             */
-/*   Updated: 2020/11/23 12:20:46 by user             ###   ########.fr       */
+/*   Updated: 2020/11/24 21:34:21 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int			hard_coord(t_parser *parser, t_token **token, int tok_num, int c)
 		(*token)->point.token = tok_num;
 		if (check->next != NULL)
 			(*token)->point.row = check->next->point.row;
-		/*else
+		else
 		{
 			printf("fof\n");
 			if ((*token)->point.row == 1)
@@ -42,7 +42,7 @@ int			hard_coord(t_parser *parser, t_token **token, int tok_num, int c)
 			else
 				(*token)->type = UNKNOWN;
 			break ;
-		}*/
+		}
 		if ((*token)->type == LABEL_TYPE && (*token)->next->type == END_FILE)
 			c = 0;
 		add_label(&parser->labels,
