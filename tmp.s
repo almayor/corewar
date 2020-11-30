c87560a26782d965931028438344260b833d9a96
vladdez <veehimrimidal@gmail.com>
2020-11-30 18:43:30 +0300

    g

---
+++ tmp.s
@@ -0,0 +1,23 @@
+#by zaz, it's not very good, but what did you expect, rainbowdash is way better
+#2013
+
+.name		"fluttershy"
+.comment	"oh, my, what a scary project"
+
+
+live	%42
+entry:
+	st		r1, 6
+	live	%42
+
+	st		r1, 6
+	live	%42
+
+
+ardeftgt:
+
+ardefgeni:
+	st 	
+		r1	, 6
+
+ardeftgt54:
